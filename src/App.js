0deaae48e7c3c18fc377c599bc31c9f86c99e93b
marcinoles87 <marcinoles87@gmail.com>
2023-05-09 20:50:14 +0200

    start code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+        <div className="weather-card">
+
+        </div>
     </div>
   );
 }
