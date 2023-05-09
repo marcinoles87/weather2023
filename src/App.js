055bd2fa96d025ea4f49f8bee96b366cf754a790
marcinoles87 <marcinoles87@gmail.com>
2023-05-09 20:51:56 +0200

    add weather card

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
         <div className="weather-card">
-
+            <input placeholder="find your countries..."></input>
+            <h2>Country Name</h2>
+            <h1> 20 C</h1>
+            <p> Today is : Sunny</p>
         </div>
     </div>
   );
