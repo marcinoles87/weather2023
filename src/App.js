887686bb32c248aefa5b162c339bb8dec7cf2078
marcinoles87 <marcinoles87@gmail.com>
2023-05-10 16:24:44 +0200

    work with fetch data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React , {useEffect, useState} from 'react'
 function App() {
 
   const [data, setData] = useState([])
+  const [city , setCity] = useState('')
+  const [temp , setTemp] = useState(0)
 
   useEffect( () => {
     fetch("https://danepubliczne.imgw.pl/api/data/synop")
@@ -22,12 +24,17 @@ function App() {
     </div>
           
   })
-  
+
+  const handleOnChange = (e) =>{
+    e.preventDefault()
+    const valueInput = e.target.value
+    
+  }
   return (
     <div className="App">
       <h1>Weather App</h1>
         <div className="weather-card">
-            <input placeholder="find your countries..."></input>
+            <input placeholder="find your countries..." onChange={handleOnChange}></input>
             <h2>Country Name {}</h2>
             <h1> 20 C</h1>
             <p> Today is : Sunny</p>
